80bbbb6cc7fbd4338b0ffee8705282ca41fc6b32
Simone Pescina <s.pescina@gmail.com>
2014-03-03 10:20:43 +0100

    no message

---
+++ app/lang/en/test.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	'section' => array(
+		'menu' => 'Tests',
+		'title' => 'Test management',
+		'subtitle' => 'Sed nec est non nisi accumsan auctor. Aenean quam metus, aliquet id fringilla vitae, sodales id orci.'
+	),
+	'listing' => array(
+		'nickname' => 'Nickname'
+	),
+	'form' => array(
+		'nickname' => 'Nickname',
+		'first_name' => 'First Name',
+		'last_name' => 'Last Name'
+	),
+	'help' => array(),
+	'panels' => array()
+);
