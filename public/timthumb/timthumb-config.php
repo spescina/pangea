74adbf75ff42504534dec615a054b4666ae6a11b
Simone Pescina <s.pescina@gmail.com>
2014-03-20 02:55:33 +0100

    no message

---
+++ public/timthumb/timthumb-config.php
@@ -0,0 +1,21 @@
+<?php
+
+define ('DEBUG_ON', false);
+
+define ('DEBUG_LEVEL', 3);
+
+define ('FILE_CACHE_MAX_FILE_AGE', 86400);
+
+define ('FILE_CACHE_SUFFIX', '.timthumb.txt');
+
+define ('FILE_CACHE_PREFIX', 'timthumb');
+
+define ('FILE_CACHE_DIRECTORY', './cache');
+
+define ('NOT_FOUND_IMAGE', './nophoto.gif');
+
+define ('ERROR_IMAGE', './nophoto.gif');
+
+define ('PNG_IS_TRANSPARENT', FALSE);
+
+define ('DEFAULT_Q', 90);
