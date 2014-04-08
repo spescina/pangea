cf31b7b57e23906f82769d0dcea6b1262daff5e2
Simone Pescina <s.pescina@gmail.com>
2014-04-08 18:44:21 +0200

    changed environment detection. use ENV environment variable

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('web'),
-
-));
+$env = $app->detectEnvironment(function() {
+        return getenv('ENV') ?: 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
